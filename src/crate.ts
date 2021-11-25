7d5cb7c2308b1b168cdea949838b93143b2352a4
Enyng <susikovvlad@gmail.com>
2021-11-25 18:19:08 +0400

    boxAndTurn

---
+++ src/crate.ts
@@ -0,0 +1,47 @@
+export class Crate extends Entity {
+	isGrabbed: boolean = false
+ 
+	constructor( transform: Transform) {
+	  super()
+	  engine.addEntity(this)
+	  this.addComponent(new BoxShape() )
+	  this.addComponent(transform)
+	  
+	  this.addComponent(
+		 new OnPointerDown(
+			() => {
+			   if (!this.isGrabbed) {
+					this.isGrabbed = true
+			  
+					// Calculates the crate's position relative to the camera
+					transform.position = Vector3.Zero()
+					transform.rotation = Quaternion.Zero()
+					transform.position.z += 1.5
+					this.setParent(Attachable.FIRST_PERSON_CAMERA)
+				 } else {
+					this.isGrabbed = false
+				  //  putDownSound.getComponent(AudioSource).playOnce()
+			  
+					// Calculate crate's ground position
+					this.setParent(null) // Remove parent
+					let forwardVector: Vector3 = Vector3.Forward()
+					  .scale(1.5)
+					  .rotate(Camera.instance.rotation)
+					transform.position = Camera.instance.position.clone().add(forwardVector)
+					transform.lookAt(Camera.instance.position)
+					transform.rotation.x = 0
+					transform.rotation.z = 0
+					let distance = Vector3.Down().scale(.1)
+					transform.translate(distance)
+					// transform.position.y = 0.5
+				 }
+			},
+			{
+			  button: ActionButton.PRIMARY,
+			  hoverText: "Pick Up / Put Down",
+			  distance: 5
+			}
+		 )
+	  )
+	}
+ }
--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,27 @@
 import Squad from "./squad";
 import Control from "./control";
 import BaseEntity from "./baseEntity";
-
+import { Crate } from './crate'
 const squid = new Squad(new GLTFShape('models/squid.glb'), { position: new Vector3(6, 0, 8) });
 const wall = new BaseEntity(new GLTFShape('models/wall10.glb'),{ position: new Vector3(3.8, 0, 8.1) });
 
 const control1 = new Control({ position: new Vector3(5.5, -5, 3.6), rotation: Quaternion.Euler(0, 90, 0) });
 const control2 = new Control({ position: new Vector3(7.5, -5, 3.6), rotation: Quaternion.Euler(0, 90, 0) });
+const controlLeft = new Control({ position: new Vector3(3.5, -5, 3.6), rotation: Quaternion.Euler(0, 90, 0) });
+const controlRight = new Control({ position: new Vector3(9.5, -5, 3.6), rotation: Quaternion.Euler(0, 90, 0) });
+// const followTheCamera = new Entity()
 
-Input.instance.subscribe("BUTTON_DOWN", ActionButton.PRIMARY, false, (e) => log('key down', e))
+// control1.addComponent(
+//   new OnPointerDown((): void => {
+//     squid.forward()
+//   })
+// )
 
 control1.addComponent(
   new OnPointerDown((): void => {
     squid.startMove(1)
   })
 )
-
 control1.addComponent(
   new OnPointerUp((): void => {
     squid.stopMove()
@@ -33,3 +39,68 @@ control2.addComponent(
     squid.stopMove()
   })
 )
+controlRight.addComponent(
+	new OnPointerDown((): void => {
+	  squid.turnR()
+	},)
+ )
+ controlRight.addComponent(
+	new OnPointerUp((): void => {
+	 squid.turnStop();
+	
+	})
+ )
+ controlLeft.addComponent(
+	new OnPointerDown((): void => {
+	  squid.turnL()
+	},)
+ )
+ 
+ controlLeft.addComponent(
+	new OnPointerUp((): void => {
+	 squid.turnStop();
+	 
+	})
+ )
+
+const crate = new Crate(
+  new Transform({
+    position: new Vector3(8, 0.5, 12),
+  })
+)
+const crate1 = new Crate(
+	
+  new Transform({
+    position: new Vector3(6, 0.5, 12),
+  })
+)
+const crate2 = new Crate(
+	
+  new Transform({
+    position: new Vector3(4, 0.5, 12),
+  })
+)
+
+// followTheCamera.addComponent(new BoxShape())
+// followTheCamera.addComponent(
+//   new Transform({
+//     position: new Vector3(2, 0.5, 1),
+//   })
+// )
+// engine.addEntity(followTheCamera)
+// let take = false;
+
+// followTheCamera.addComponent(
+// 	new OnClick(():void=>{
+// 		if(!take){
+// 		followTheCamera.setParent(Attachable.FIRST_PERSON_CAMERA);
+// 		take = true
+// 	} else {
+// 		followTheCamera.setParent('');
+// 	}
+		
+// 	})
+// )
+
+// Input.instance.subscribe("BUTTON_DOWN", ActionButton.PRIMARY, false, (e) => log('key down', e))
+// { button: ActionButton.PRIMARY }
